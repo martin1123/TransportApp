8379b688755a352673a1d63c4a95e82521e2d7c9
melibaran <melibaran4@gmail.com>
2025-06-09 09:55:32 -0300

    Hago modificaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
  import React from 'react'
- import { Routes, Route } from 'react-router-dom'
+ import { BrowserRouter, Routes, Route } from 'react-router-dom'
  import Login from './pages/Login'
  import Register from './pages/Register'
  import Dashboard from './pages/Dashboard'
+ import TripForm from './pages/TripForm'
+ import RentabilityCalculator from './components/RentabilityCalculator'
+ import Navigation from './components/Navigation'
+ import ProtectedRoute from './components/ProtectedRoute'
 
  function App() {
    return (
-    <Routes>
-       <Route path="/login" element={<Login />} />
-       <Route path="/register" element={<Register />} />
-       <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-    </Routes>
+     <BrowserRouter>
+       <Navigation />
+       <Routes>
+         <Route path="/login" element={<Login />} />
+         <Route path="/register" element={<Register />} />
+         <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+         <Route path="/viaje" element={<ProtectedRoute><TripForm /></ProtectedRoute>} />
+         <Route path="/rentabilidad" element={<ProtectedRoute><RentabilityCalculator /></ProtectedRoute>} />
+       </Routes>
+     </BrowserRouter>
    )
  }
 
