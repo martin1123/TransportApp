d457f87601bad1f0b33a35bb99f79e3732a030d0
melissaBaran <melibaran4@gmail.com>
2025-06-24 10:39:07 -0300

    subo variables de entorno

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,15 +1,15 @@
 import { createClient } from '@supabase/supabase-js'
 
-const VITE_SUPABASE_URL = 'https://bmwvtorislhpxqdjfyul.supabase.co'
-const VITE_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtd3Z0b3Jpc2xocHhxZGpmeXVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5MjkyOTksImV4cCI6MjA2NTUwNTI5OX0.RFljOWx0chqv4NLq9uQsoNAZZtdWRM2xsv1cvE42L4g'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 // Verificación de que las variables de entorno estén configuradas
-if (!VITE_SUPABASE_URL || !VITE_SUPABASE_ANON_KEY) {
+if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Faltan las variables de entorno de Supabase. Verifica VITE_SUPABASE_URL y VITE_SUPABASE_ANON_KEY')
 }
 
 
-export const supabase = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY, {
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     // Configuración de autenticación
     autoRefreshToken: true, 
